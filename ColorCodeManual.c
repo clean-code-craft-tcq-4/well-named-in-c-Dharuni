0b8aafdcc9d3a1e15e7b321676879385ebac79d9
Belavatagi-Dharuni <34068379+Dharuni@users.noreply.github.com>
2022-06-15 09:08:30 +0530

    Update ColorCodeManual.c

--- ColorCodeManual.c
+++ ColorCodeManual.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
+#include <assert.h>
 #include "main.h"
+#include "task.h"
+
+void colorCodingReferenceManual(void)
+{
+    int majorColorCount,minorColorCount;
+    ColorPair colorPairReference;
+    char colorPairManual[MAX_COLORPAIR_NAME_CHARS];
+    printf("ColorPair Coding Reference Manual\n");
+    for(majorColorCount=0;majorColorCount<numberOfMajorColors;majorColorCount++)
+    {
+        for(minorColorCount=0;minorColorCount<numberOfMinorColors;minorColorCount++)
+        {
+            colorpairreference.majorColor = (MajorColor)majorColorCount;
+            colorpairreference.minorColor = (MinorColor)minorColorCount;
+            int pairNumberReference = GetPairNumberFromColor(&colorPairReference);
+            ColorPairToString(&colorPairReference, colorPairManual);
+            printf(" %s\t%d\n", colorPairManual,pairNumberReference );
+
+        }
+    }
+}
+

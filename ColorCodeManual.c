eddb634a808d84e10008b23f5455a7e2bf0ea0c5
Belavatagi-Dharuni <34068379+Dharuni@users.noreply.github.com>
2022-06-15 10:10:37 +0530

    Update ColorCodeManual.c

--- ColorCodeManual.c
+++ ColorCodeManual.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
-#include <assert.h>
-#include "task.h"
+// #include <stdio.h>
+// #include <assert.h>
+// #include "task.h"
 
-void colorCodingReferenceManual(void)
-{
-    int majorColorCount,minorColorCount;
-    ColorPair colorPairReference;
-    char colorPairManual[MAX_COLORPAIR_NAME_CHARS];
-    printf("ColorPair Coding Reference Manual\n");
-    for(majorColorCount=0;majorColorCount<numberOfMajorColors;majorColorCount++)
-    {
-        for(minorColorCount=0;minorColorCount<numberOfMinorColors;minorColorCount++)
-        {
-            colorPairReference.majorColor = (MajorColor)majorColorCount;
-            colorPairReference.minorColor = (MinorColor)minorColorCount;
-            int pairNumberReference = GetPairNumberFromColor(&colorPairReference);
-            ColorPairToString(&colorPairReference, colorPairManual);
-            printf(" %s\t%d\n", colorPairManual,pairNumberReference );
+// void colorCodingReferenceManual(void)
+// {
+//     int majorColorCount,minorColorCount;
+//     ColorPair colorPairReference;
+//     char colorPairManual[MAX_COLORPAIR_NAME_CHARS];
+//     printf("ColorPair Coding Reference Manual\n");
+//     for(majorColorCount=0;majorColorCount<numberOfMajorColors;majorColorCount++)
+//     {
+//         for(minorColorCount=0;minorColorCount<numberOfMinorColors;minorColorCount++)
+//         {
+//             colorPairReference.majorColor = (MajorColor)majorColorCount;
+//             colorPairReference.minorColor = (MinorColor)minorColorCount;
+//             int pairNumberReference = GetPairNumberFromColor(&colorPairReference);
+//             ColorPairToString(&colorPairReference, colorPairManual);
+//             printf(" %s\t%d\n", colorPairManual,pairNumberReference );
 
-        }
-    }
-}
+//         }
+//     }
+// }
 

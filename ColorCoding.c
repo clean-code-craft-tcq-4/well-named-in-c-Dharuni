a64cefb144d7807873aba5dbadaf4e89cec107b2
Belavatagi-Dharuni <34068379+Dharuni@users.noreply.github.com>
2022-06-15 10:16:21 +0530

    Update ColorCoding.c

--- ColorCoding.c
+++ ColorCoding.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
-#include "task.h"
+#include "ColorCoding.h"
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",

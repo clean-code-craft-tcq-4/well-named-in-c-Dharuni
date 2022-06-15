7845634f4324f5653c06e93245ce5cc8c89f7df2
Belavatagi-Dharuni <34068379+Dharuni@users.noreply.github.com>
2022-06-15 09:34:28 +0530

    Update task.h

--- task.h
+++ task.h
@@ -1,8 +1,8 @@
 #include "main.h"
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
+// const char* MajorColorNames[] = {
+//     "White", "Red", "Black", "Yellow", "Violet"
+// };
 int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 const char* MinorColorNames[] = {
